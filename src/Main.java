077ba8da1485d5234df71d8ea575db630f35d28f
Julia Shubartsova <rybenceva.ya@gmail.com>
2020-12-01 15:38:18 +0300

    src

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price;
+        price = 15_000; // цена билета
+        int mile;
+        mile = 20; // цена 1 мили
+        int total = price / mile;
+        System.out.println(total);
+    }
+}
